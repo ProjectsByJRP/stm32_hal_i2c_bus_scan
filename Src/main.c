9d3412d3743563e9d6444335e554e559cc06a00c
ProjectsByJRP <ProjectsByJRP@users.noreply.github.com>
2017-05-14 06:24:06 -0500

    Commented for clarity

--- Src/main.c
+++ Src/main.c
@@ -61,9 +61,9 @@ static void MX_I2C1_Init(void);
 
 /* USER CODE BEGIN PFP */
 /* Private function prototypes -----------------------------------------------*/
+
+/* Direct printf to output somewhere */
 #ifdef __GNUC__
-/* With GCC, small printf (option LD Linker->Libraries->Small printf
-   set to 'Yes') calls __io_putchar() */
 #define PUTCHAR_PROTOTYPE int __io_putchar(int ch)
 #else
 #define PUTCHAR_PROTOTYPE int fputc(int ch, FILE *f)
@@ -111,6 +111,11 @@ int main(void)
 
   /* USER CODE BEGIN 2 */
   char uart2Data[24] = "Connected to UART Two\r\n";
+   /*
+    * Output to uart2
+    * use screen or putty or whatever terminal software
+    * 8N1 115200
+    */
    HAL_UART_Transmit(&huart2, (uint8_t *)&uart2Data,sizeof(uart2Data), 0xFFFF);
 
  	printf("\r\n");
@@ -120,14 +125,21 @@ int main(void)
  	uint8_t i;
  	for (i=1; i<128; i++)
  	{
+ 	  /*
+ 	   * the HAL wants a left aligned i2c address
+ 	   * &hi2c1 is the handle
+ 	   * (uint16_t)(i<<1) is the i2c address left aligned
+ 	   * retries 2
+ 	   * timeout 2
+ 	   */
  	  result = HAL_I2C_IsDeviceReady(&hi2c1, (uint16_t)(i<<1), 2, 2);
- 	  if (result != HAL_OK)
+ 	  if (result != HAL_OK) // HAL_ERROR or HAL_BUSY or HAL_TIMEOUT
  	  {
- 		  printf(".");
+ 		  printf("."); // No ACK received at that address
  	  }
  	  if (result == HAL_OK)
  	  {
- 		  printf("0x%X", i);
+ 		  printf("0x%X", i); // Received an ACK at that address
  	  }
  	}
  	printf("\r\n");
